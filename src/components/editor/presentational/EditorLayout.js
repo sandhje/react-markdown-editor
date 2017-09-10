5db50a749eda1f8c6947da7ad632b368213474a6
Sandhjé Bouw <sandhje@gmail.com>
2017-09-10 23:27:21 +0200

    Add monaco editor to app layout

--- src/components/editor/presentational/EditorLayout.js
+++ src/components/editor/presentational/EditorLayout.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import MonacoEditor from 'react-monaco-editor';
 import "../assets/Editor.css";
 
 class EditorLayout extends Component {
+    editorDidMount(editor, monaco) {
+        console.log('editorDidMount', editor);
+        editor.focus();
+    }
+
     render() {
         console.log(this.props);
         return (
             <div className="Editor">
-                <textarea value={this.props.text} onChange={this.props.handleChange} />
+                <MonacoEditor
+                    width="100%"
+                    height="100%"
+                    language="markdown"
+                    theme="vs-light"
+                    value={this.props.text}
+                    onChange={this.props.handleChange}
+                    editorDidMount={this.editorDidMount}
+                />
             </div>
         );
     }
